aa2fb66619330aacdfaedfc271a420820e4e26ae
Muhammad Sadewa Wicaksana W <wicaksanasadewa@yahoo.com>
2020-11-19 19:51:59 +0700

    Adding Space

--- app/src/main/java/com/flashcom/meetup3/DashboardActivity.kt
+++ app/src/main/java/com/flashcom/meetup3/DashboardActivity.kt
@@ -13,6 +13,7 @@ class DashboardActivity : AppCompatActivity() {
     }
 
     fun calculate(view: View) {
+
         val result = ed_db_panjang.text.toString().toDouble() * ed_db_lebar.text.toString().toDouble()
         Toast.makeText(this, "$result", Toast.LENGTH_SHORT).show()
     }
